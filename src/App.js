2b40f7c75a552ea4ae896125c67d0d092c214eb2
Alan Pecenkovic <alanquad@yahoo.com>
2022-11-08 15:11:22 +0100

    router and pages setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Create from "./pages/create/Create";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/login/Login";
+import Project from "./pages/project/Project";
+import Signup from "./pages/signup/Signup";
 
 function App() {
-  return <div className="App">Kunai Company</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route path="/" exact>
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/projects/:id">
+              <Project />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
