2f8dd1175d279adba391d5b7ff2f89e226714abe
Alan Pecenkovic <alanquad@yahoo.com>
2022-11-17 16:27:39 +0100

    setting form errors

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -20,6 +20,7 @@ function Create() {
   const [dueDate, setDueDate] = useState();
   const [category, setCategory] = useState();
   const [assignedUsers, setAssignedUsers] = useState([]);
+  const [formError, setFormError] = useState(null);
 
   useEffect(() => {
     if (documents) {
@@ -32,6 +33,17 @@ function Create() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError(null);
+
+    if (!category) {
+      setFormError("Please select a project category");
+      return;
+    }
+    if (assignedUsers.length < 1) {
+      setFormError("Please assign the project to at least one user");
+      return;
+    }
+
     console.log(name, details, dueDate, category.value, assignedUsers);
   };
 
@@ -82,6 +94,8 @@ function Create() {
           />
         </label>
         <button className="btn">Add Project</button>
+
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
