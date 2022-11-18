9189e9f6b8561c46bac3e54266085c018af22419
Alan Pecenkovic <alanquad@yahoo.com>
2022-11-18 10:14:25 +0100

    making a grid of projects

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Avatar from "./Avatar";
+
 import "./ProjectList.css";
 
 function ProjectList({ projects }) {
   return (
-    <div>
-      <h2>Projects</h2>
+    <div className="project-list">
       {projects.length === 0 && <p>There are no projects yet!</p>}
       {projects.map((project) => (
-        <div key={project.id}>{project.name}</div>
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   );
