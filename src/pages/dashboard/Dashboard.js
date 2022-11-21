41ea08b63e571a5e0d0db22ff5c924ec3c83e55d
Alan Pecenkovic <alanquad@yahoo.com>
2022-11-21 14:06:33 +0100

    filtering projects

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,14 +4,41 @@ import { useCollection } from "../../hooks/useCollection";
 import ProjectFilter from "./ProjectFilter";
 
 import "./Dashboard.css";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 function Dashboard() {
+  const { user } = useAuthContext();
   const { documents, error } = useCollection("projects");
   const [currentFilter, setCurrentFilter] = useState("all");
 
   const changeFilter = (newFilter) => {
     setCurrentFilter(newFilter);
   };
+
+  const projects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case "all":
+            return true;
+          case "mine":
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case "development":
+          case "design":
+          case "sales":
+          case "marketing":
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <div>
@@ -23,7 +50,7 @@ function Dashboard() {
           changeFilter={changeFilter}
         />
       )}
-      {documents && <ProjectList projects={documents} />}
+      {projects && <ProjectList projects={projects} />}
     </div>
   );
 }
